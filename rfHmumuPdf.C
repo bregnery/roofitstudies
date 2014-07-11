2573b15be523872a7a452276c880a710539b2d46
Brendan Regnery <bregnery@localhost.localdomain>
2014-07-11 14:08:52 -0400

    Fixed Expo multiplication Bug

--- rfHmumuPdf.C
+++ rfHmumuPdf.C
@@ -23,7 +23,8 @@ void rfHmumuPdf(){
 	RooRealVar coef("coef","coefficent for Addition",0,0,1);	
 
 	// Declare observable
-	RooRealVar x("x","x",60,160);
+	RooRealVar x("x","x",110,160);
+	//x.setRange("zPeak",90,92);
 
 	// Declare Parameters for Breit Wigner
 	RooRealVar mean("mean","mean of Breit Wigner",91.1876);
@@ -31,19 +32,33 @@ void rfHmumuPdf(){
 	
 	//Declare Parameter for Exponential
 	RooRealVar lambda("lambda","lambda of Exponential",-0.001,-0.1,-0.0001);
+	/*
+		// Construct a nonrealitivistic Breit Wigner
+		//RooBreitWigner bWpdf("bWpdf","bWpdf",x,mean,sigma);
+		RooGenericPdf bWpdf("bWpdf","bWpdf","(@2/((@0-@1)*(@0-@1)+(@2/2)*(@2/2)))",RooArgList(x,mean,sigma));
 	
+		// Construct a realitivistic Breit Wigner
+		RooGenericPdf relBW("relBW","relBW","((@0*@0)/((@0*@0-(@1*@1))*(@0*@0-(@1*@1))+(@1*@1*@2*@2)))",RooArgList(x,mean,sigma));
+
+		// Construct a Z Breit Wigner
+		RooGenericPdf zBWpdf("zBWpdf","zBWpdf","((@0*@0)/((@0*@0-(@1*@1))*(@0*@0-(@1*@1))+((@0*@0*@0*@0*@2*@2)/(@1*@1))))",RooArgList(x,mean,sigma));
+	
+		// Create a Photon Pdf
+		RooGenericPdf phoPdf("phoPdf","phoPdf","(1/(@0*@0))",RooArgList(x));
+	*/
+
 	// Construct a nonrealitivistic Breit Wigner
 	//RooBreitWigner bWpdf("bWpdf","bWpdf",x,mean,sigma);
-	RooGenericPdf bWpdf("bWpdf","bWpdf","(@2/((@0-@1)*(@0-@1)+(@2/2)*(@2/2)))",RooArgList(x,mean,sigma));
+	RooGenericPdf bWpdf("bWpdf","bWpdf","(@2/((@0-@1)*(@0-@1)+(@2/2)*(@2/2)))*TMath::Exp(@3*@0)",RooArgList(x,mean,sigma,lambda));
 	
 	// Construct a realitivistic Breit Wigner
-	RooGenericPdf relBW("relBW","relBW","((@0*@0)/((@0*@0-(@1*@1))*(@0*@0-(@1*@1))+(@1*@1*@2*@2)))",RooArgList(x,mean,sigma));
+	RooGenericPdf relBW("relBW","relBW","((@0*@0)/((@0*@0-(@1*@1))*(@0*@0-(@1*@1))+(@1*@1*@2*@2)))*TMath::Exp(@3*@0)",RooArgList(x,mean,sigma,lambda));
 
 	// Construct a Z Breit Wigner
-	RooGenericPdf zBWpdf("zBWpdf","zBWpdf","((@0*@0)/((@0*@0-(@1*@1))*(@0*@0-(@1*@1))+((@0*@0*@0*@0*@2*@2)/(@1*@1))))",RooArgList(x,mean,sigma));
+	RooGenericPdf zBWpdf("zBWpdf","zBWpdf","((@0*@0)/((@0*@0-(@1*@1))*(@0*@0-(@1*@1))+((@0*@0*@0*@0*@2*@2)/(@1*@1))))*TMath::Exp(@3*@0)",RooArgList(x,mean,sigma,lambda));
 	
 	// Create a Photon Pdf
-	RooGenericPdf phoPdf("phoPdf","phoPdf","(1/(@0*@0))",RooArgList(x));
+	RooGenericPdf phoPdf("phoPdf","phoPdf","(1/(@0*@0))*TMath::Exp(@1*@0)",RooArgList(x,lambda));
 	
 	// Add the Photon Term to the other Pdfs
 	RooAddPdf phoBW("phoBW","Photon term added to a Breit Wigner",phoPdf,bWpdf,coef);
@@ -53,20 +68,26 @@ void rfHmumuPdf(){
 	// Construct an Exponential Pdf
 	RooExponential expo("expo","exponential PDF",x,lambda);
 	
-	// Multiply the exponential by the other PDFs
-	RooProdPdf exPhoBW("exPhoBW","Exponential multiplied to BW",phoBW,expo);
-	RooProdPdf exPhoRBW("exPhoRBW","Expo multiplied to a Relativistic BW",phoRelBW,expo);
-	RooProdPdf exPhoZBW("exPhoZBW","Expo multiplied to a Z BW",phoZBW,expo);
+	/*
+		// Multiply the exponential by the other PDFs
+		RooProdPdf exPhoBW("exPhoBW","Exponential multiplied to BW",phoBW,expo);
+	       	 exPhoRBWArgSet = RooArgSet(phoRelBW,expo);
+		RooProdPdf exPhoRBW("exPhoRBW","Expo multiplied to a Relativistic BW",exPhoRBWArgSet,Conditional(exPhoRBWArgSet,x));
+		RooProdPdf exPhoZBW("exPhoZBW","Expo multiplied to a Z BW",phoZBW,expo);
+	*/
 	
 	// Test by generating a data set
-	RooDataSet* data = exPhoBW.generate(x,1000);
+	RooDataSet* data = phoBW.generate(x,10000);
 	
 	// Plot and draw on a canvas
 	RooPlot* xframe = x.frame(Title("Breit Wigner Plots"));
 	data->plotOn(xframe);
-	exPhoBW.plotOn(xframe);
-	exPhoRBW.plotOn(xframe,LineColor(kGreen),LineStyle(kDashed));
-	exPhoZBW.plotOn(xframe,LineColor(kOrange),LineStyle(kDashed));
+	//exPhoBW.plotOn(xframe,Range("zPeak"));
+	//exPhoRBW.plotOn(xframe,LineColor(kGreen),LineStyle(kDashed));
+	//exPhoZBW.plotOn(xframe,LineColor(kOrange),LineStyle(kDashed));
+	phoBW.plotOn(xframe,Range("zPeak"));
+	phoRelBW.plotOn(xframe,LineColor(kGreen),LineStyle(kDashed));
+	phoZBW.plotOn(xframe,LineColor(kOrange),LineStyle(kDashed));
 	TCanvas* c = new TCanvas("rfHmumuPdf","rfHmumuPdf",800,400);
 	xframe->Draw();
 }

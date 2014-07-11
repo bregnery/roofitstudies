538681ac29ac618d391835d495f59f9e638e12aa
Brendan Regnery <bregnery@localhost.localdomain>
2014-07-11 14:30:15 -0400

    Added an FFT convolution with a gaussian with 0.5 buffer fraction

--- rfHmumuPdf.C
+++ rfHmumuPdf.C
@@ -9,6 +9,7 @@
 #include "RooDataSet.h"
 #include "RooBreitWigner.h"
 #include "RooExponential.h"
+#include "RooGaussian.h"
 #include "TCanvas.h"
 #include "TAxis.h"
 #include "RooPlot.h"
@@ -30,8 +31,12 @@ void rfHmumuPdf(){
 	RooRealVar mean("mean","mean of Breit Wigner",91.1876);
 	RooRealVar sigma("sigma","width of Breit Wigner",2.4952);
 	
-	//Declare Parameter for Exponential
+	// Declare Parameter for Exponential
 	RooRealVar lambda("lambda","lambda of Exponential",-0.001,-0.1,-0.0001);
+	
+	// Declare Parameters for Gaussian
+	RooRealVar meanG("meanG","mean of Gaussian",0);
+	RooRealVar sigmaG("sigmaG","width of Gaussian",3);
 	/*
 		// Construct a nonrealitivistic Breit Wigner
 		//RooBreitWigner bWpdf("bWpdf","bWpdf",x,mean,sigma);
@@ -65,10 +70,13 @@ void rfHmumuPdf(){
 	RooAddPdf phoRelBW("phoRelBW","Photon added to a relativistic Breit Wigner",phoPdf,relBW,coef);
 	RooAddPdf phoZBW("phoZBW","Photon added to a Z Breit Wigner",phoPdf,zBWpdf,coef);
 	
-	// Construct an Exponential Pdf
-	RooExponential expo("expo","exponential PDF",x,lambda);
+	// Create a Gaussian Pdf
+	RooGaussian gaus("gaus","gaus",x,meanG,sigmaG);
 	
-	/*
+	/*	
+		// Construct an Exponential Pdf
+		RooExponential expo("expo","exponential PDF",x,lambda);
+	
 		// Multiply the exponential by the other PDFs
 		RooProdPdf exPhoBW("exPhoBW","Exponential multiplied to BW",phoBW,expo);
 	       	 exPhoRBWArgSet = RooArgSet(phoRelBW,expo);
@@ -76,8 +84,17 @@ void rfHmumuPdf(){
 		RooProdPdf exPhoZBW("exPhoZBW","Expo multiplied to a Z BW",phoZBW,expo);
 	*/
 	
+	// Convolute Pdfs with Gaussian
+	x.setBins(1000,"cache");
+	RooFFTConvPdf phoBWxg("phoBWxg","phoBW (x) gaus",x,phoBW,gaus);
+	phoBWxg.setBufferFraction(0.5);
+	RooFFTConvPdf phoRelBWxg("phoRelBWxg","phoRelBW (x) gaus",x,phoRelBW,gaus);
+	phoRelBWxg.setBufferFraction(0.5);
+	RooFFTConvPdf phoZBWxg("phoZBWxg","phoZBW (x) gaus",x,phoZBW,gaus);
+	phoZBWxg.setBufferFraction(0.5);
+	
 	// Test by generating a data set
-	RooDataSet* data = phoBW.generate(x,10000);
+	RooDataSet* data = phoBWxg.generate(x,10000);
 	
 	// Plot and draw on a canvas
 	RooPlot* xframe = x.frame(Title("Breit Wigner Plots"));
@@ -85,9 +102,9 @@ void rfHmumuPdf(){
 	//exPhoBW.plotOn(xframe,Range("zPeak"));
 	//exPhoRBW.plotOn(xframe,LineColor(kGreen),LineStyle(kDashed));
 	//exPhoZBW.plotOn(xframe,LineColor(kOrange),LineStyle(kDashed));
-	phoBW.plotOn(xframe,Range("zPeak"));
-	phoRelBW.plotOn(xframe,LineColor(kGreen),LineStyle(kDashed));
-	phoZBW.plotOn(xframe,LineColor(kOrange),LineStyle(kDashed));
+	phoBWxg.plotOn(xframe,Range("zPeak"));
+	phoRelBWxg.plotOn(xframe,LineColor(kGreen),LineStyle(kDashed));
+	phoZBWxg.plotOn(xframe,LineColor(kOrange),LineStyle(kDashed));
 	TCanvas* c = new TCanvas("rfHmumuPdf","rfHmumuPdf",800,400);
 	xframe->Draw();
 }

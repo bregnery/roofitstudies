a0dc7d5e533a744018616ff8d78997cc88803066
Brendan Regnery <bregnery@localhost.localdomain>
2014-07-11 15:15:01 -0400

    Added interference term

--- rfHmumuPdf.C
+++ rfHmumuPdf.C
@@ -21,7 +21,8 @@ using namespace RooFit;
 
 void rfHmumuPdf(){
 	// Declare Addition Coefficent
-	RooRealVar coef("coef","coefficent for Addition",0,0,1);	
+	RooRealVar coefPho("coefPho","coefficent for Addition",0.88,0,1);
+	RooRealVar coefBW("coefBW","coefficent for BW addition",0.11,0,1);	
 
 	// Declare observable
 	RooRealVar x("x","x",110,160);
@@ -65,10 +66,18 @@ void rfHmumuPdf(){
 	// Create a Photon Pdf
 	RooGenericPdf phoPdf("phoPdf","phoPdf","(1/(@0*@0))*TMath::Exp(@1*@0)",RooArgList(x,lambda));
 	
+	// Create an interference Pdf
+	RooGenericPdf infPdf("infPdf","infPdf","((@0*@0-(@1*@1))/((@0*@0-(@1*@1))*(@0*@0-(@1*@1))+(@0*@0*@0*@0*@2*@2)/(@1*@1)))*TMath::Exp(@3*@0)",RooArgList(x,mean,sigma,lambda));
+	
 	// Add the Photon Term to the other Pdfs
-	RooAddPdf phoBW("phoBW","Photon term added to a Breit Wigner",phoPdf,bWpdf,coef);
-	RooAddPdf phoRelBW("phoRelBW","Photon added to a relativistic Breit Wigner",phoPdf,relBW,coef);
-	RooAddPdf phoZBW("phoZBW","Photon added to a Z Breit Wigner",phoPdf,zBWpdf,coef);
+	RooAddPdf phoBW("phoBW","Photon term added to a Breit Wigner",phoPdf,bWpdf,coefPho);
+	RooAddPdf phoRelBW("phoRelBW","Photon added to a relativistic Breit Wigner",phoPdf,relBW,coefPho);
+	RooAddPdf phoZBW("phoZBW","Photon added to a Z Breit Wigner",phoPdf,zBWpdf,coefPho);
+
+	// Add the Interference Term to the Pdfs
+	RooAddPdf phoBWin("phoBWin","PhoBW added to an interference term",RooArgList(phoPdf,bWpdf,infPdf),RooArgList(coefPho,coefBW));
+	RooAddPdf phoRelBWin("phoRelBWin","phoRelBW added to an interference term",RooArgList(phoPdf,relBW,infPdf),RooArgList(coefPho,coefBW));
+	RooAddPdf phoZBWin("phoZBWin","phoZBW added to an interference term",RooArgList(phoPdf,zBWpdf,infPdf),RooArgList(coefPho,coefBW));
 	
 	// Create a Gaussian Pdf
 	RooGaussian gaus("gaus","gaus",x,meanG,sigmaG);
@@ -86,11 +95,11 @@ void rfHmumuPdf(){
 	
 	// Convolute Pdfs with Gaussian
 	x.setBins(1000,"cache");
-	RooFFTConvPdf phoBWxg("phoBWxg","phoBW (x) gaus",x,phoBW,gaus);
+	RooFFTConvPdf phoBWxg("phoBWxg","phoBWin (x) gaus",x,phoBWin,gaus);
 	phoBWxg.setBufferFraction(0.5);
-	RooFFTConvPdf phoRelBWxg("phoRelBWxg","phoRelBW (x) gaus",x,phoRelBW,gaus);
+	RooFFTConvPdf phoRelBWxg("phoRelBWxg","phoRelBWin (x) gaus",x,phoRelBWin,gaus);
 	phoRelBWxg.setBufferFraction(0.5);
-	RooFFTConvPdf phoZBWxg("phoZBWxg","phoZBW (x) gaus",x,phoZBW,gaus);
+	RooFFTConvPdf phoZBWxg("phoZBWxg","phoZBWin (x) gaus",x,phoZBWin,gaus);
 	phoZBWxg.setBufferFraction(0.5);
 	
 	// Test by generating a data set
@@ -98,13 +107,17 @@ void rfHmumuPdf(){
 	
 	// Plot and draw on a canvas
 	RooPlot* xframe = x.frame(Title("Breit Wigner Plots"));
-	data->plotOn(xframe);
+	//data->plotOn(xframe);
 	//exPhoBW.plotOn(xframe,Range("zPeak"));
 	//exPhoRBW.plotOn(xframe,LineColor(kGreen),LineStyle(kDashed));
 	//exPhoZBW.plotOn(xframe,LineColor(kOrange),LineStyle(kDashed));
-	phoBWxg.plotOn(xframe,Range("zPeak"));
-	phoRelBWxg.plotOn(xframe,LineColor(kGreen),LineStyle(kDashed));
-	phoZBWxg.plotOn(xframe,LineColor(kOrange),LineStyle(kDashed));
+	phoBW.plotOn(xframe,LineColor(kRed));
+	phoRelBW.plotOn(xframe,LineColor(kYellow),LineStyle(kDashed));
+	phoZBW.plotOn(xframe,LineColor(kBlack),LineStyle(kDashed));
+	
+	phoBWin.plotOn(xframe);
+	phoRelBWin.plotOn(xframe,LineColor(kGreen),LineStyle(kDashed));
+	phoZBWin.plotOn(xframe,LineColor(kOrange),LineStyle(kDashed));
 	TCanvas* c = new TCanvas("rfHmumuPdf","rfHmumuPdf",800,400);
 	xframe->Draw();
 }
